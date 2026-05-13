7f9c94b7398fef41007c2195c3c3344e2cc91d78
1nbuhuiwan <88577438+1nbuhuiwan@users.noreply.github.com>
2026-05-13 09:13:29 +0800

    Update MainActivity.java

--- app/src/main/java/com/quizmaster/MainActivity.java
+++ app/src/main/java/com/quizmaster/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         if (webView.canGoBack()) {
             webView.goBack();
         } else {
-            super.onBackBackPressed();
+            super.onBackPressed();
         }
     }
 }
