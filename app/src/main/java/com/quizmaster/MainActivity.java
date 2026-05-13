78492a8bd206da4d8747fba99fd52e9bdff1ad5b
1nbuhuiwan <2780532601@qq.com>
2026-05-13 08:20:09 +0800

    Initial commit

---
+++ app/src/main/java/com/quizmaster/MainActivity.java
@@ -0,0 +1,37 @@
+package com.quizmaster;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        
+        // 防止跳转到外部浏览器
+        webView.setWebViewClient(new WebViewClient());
+        
+        // 加载本地 HTML 文件
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackBackPressed();
+        }
+    }
+}
